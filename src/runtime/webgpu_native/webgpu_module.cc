1f71783610aa9704d8ad70328e302f9a764e7681
tqchen <tianqi.tchen@gmail.com>
2023-03-19 15:48:38 -0400

    initial hack ok

--- src/runtime/webgpu_native/webgpu_module.cc
+++ src/runtime/webgpu_native/webgpu_module.cc
@@ -1,10 +1,15 @@
 #include <dawn/webgpu.h>
+#include <dawn/dawn_proc.h>
+#include <dawn/native/DawnNative.h>
 #include <tvm/runtime/registry.h>
 
 namespace tvm {
 namespace runtime {
 namespace webgpu {
 void Init() {
+    DawnProcTable procs = dawn::native::GetProcs();
+    dawnProcSetProcs(&procs);
+
     // 1. We create a descriptor
     WGPUInstanceDescriptor desc = {};
     desc.nextInChain = nullptr;
@@ -15,7 +20,6 @@ void Init() {
     // 3. We can check whether there is actually an instance created
     if (!instance) {
         std::cerr << "Could not initialize WebGPU!" << std::endl;
-        return 1;
     }
 
     // 4. Display the object (WGPUInstance is a simple pointer, it may be
