6e435291e380d8172004ed267e463ddc670abaa9
tqchen <tianqi.tchen@gmail.com>
2025-06-27 15:26:57 -0400

    te wip

--- src/te/operation/compute_op.cc
+++ src/te/operation/compute_op.cc
@@ -38,6 +38,11 @@ namespace tvm {
 namespace te {
 using namespace tir;
 
+TVM_FFI_STATIC_INIT_BLOCK({
+  BaseComputeOpNode::RegisterReflection();
+  ComputeOpNode::RegisterReflection();
+});
+
 TVM_STATIC_IR_FUNCTOR(ReprPrinter, vtable)
     .set_dispatch<ComputeOpNode>([](const ObjectRef& node, ReprPrinter* p) {
       auto* op = static_cast<const ComputeOpNode*>(node.get());
