2e4e6685ccde1bc84130050d873ecf5c3ee96360
tqchen <tianqi.tchen@gmail.com>
2025-07-29 12:04:10 -0400

    small str

---
+++ apps/benchmark_json.py
@@ -0,0 +1,27 @@
+import json
+import sys
+import time
+from tvm import ffi as tvm_ffi
+
+
+def benchmark_json_parse(name, func, nrepeat=2):
+    x = open(sys.argv[1], "r").read()
+    func(x)
+    start = time.time()
+    for i in range(nrepeat):
+        func(x)
+    end = time.time()
+    speed =  (end - start) / nrepeat
+    print(f"{name}: {speed} sec/iter")
+
+
+def run_benchmark():
+    ffi_parse = tvm_ffi.get_global_func("ffi.json.Parse")
+    picojson_parse = tvm_ffi.get_global_func("ffi.picojson.parse")
+    benchmark_json_parse("json.loads", json.loads)
+    benchmark_json_parse("tvm_ffi.json.Parse", ffi_parse)
+    benchmark_json_parse("tvm_ffi.picojson.parse", picojson_parse)
+
+
+if __name__ == "__main__":
+    run_benchmark()
