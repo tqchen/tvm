6c8228bbc0705d180aa78a8e5b466dbf74196ca4
tqchen <tianqi.tchen@gmail.com>
2026-06-22 23:25:09 +0000

    [REFACTOR][Relax] Use PrimType for inferred tensor dtypes

--- src/relax/transform/lazy_transform_params.cc
+++ src/relax/transform/lazy_transform_params.cc
@@ -66,7 +66,7 @@ class LazyInputMutator : public ExprMutator {
     }
 
     Var fget_param("fget_param",
-                   FuncType({PrimType(DataType::Int(64)), ObjectType()}, ObjectType()));
+                   FuncType({PrimType::Int(64), ObjectType()}, ObjectType()));
 
     ffi::Array<Var> new_params(func->params.begin(), func->params.begin() + num_input_params);
     new_params.push_back(fget_param);
@@ -145,7 +145,7 @@ class LazyOutputMutator : public ExprMutator {
       define_lookup(0, func_body->body);
     }
 
-    Var fset_output("fset_output", FuncType({PrimType(DataType::Int(64)), ObjectType()},
+    Var fset_output("fset_output", FuncType({PrimType::Int(64), ObjectType()},
                                             TupleType(ffi::Array<Type>{}), /* purity = */ false));
     plan_ = FunctionPlan{std::move(output_lookup), fset_output};
 
