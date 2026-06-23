6be809ddf21edce0fa5223224cba6be4fc44ac7e
tqchen <tianqi.tchen@gmail.com>
2026-06-23 23:45:56 +0000

    [BENCH][IR] Probe raw dtype rewrite pattern storage

--- src/arith/pattern_match.h
+++ src/arith/pattern_match.h
@@ -286,7 +286,7 @@ class PVarWithDataType : public PVarWithCheck<PVarWithDataType<T, DType>, T> {
  public:
   explicit PVarWithDataType(const DType& dtype) : dtype_(dtype) {}
 
-  bool Match_(const T& value) const { return dtype_.Match_(value.ty()); }
+  bool Match_(const T& value) const { return dtype_.Match_(value.ty()->dtype); }
 
  protected:
   typename DType::Nested dtype_;
@@ -295,15 +295,15 @@ class PVarWithDataType : public PVarWithCheck<PVarWithDataType<T, DType>, T> {
 /*!
  * \brief Pattern variable container for data type with lanes.
  */
-class PVecDataType : public PVarWithCheck<PVecDataType, PrimType> {
+class PVecDataType : public PVarWithCheck<PVecDataType, DLDataType> {
  public:
   /*! \brief construct vector dtype placeholder with element type check */
-  explicit PVecDataType(PrimType elem_dtype) : elem_dtype_(elem_dtype) {}
+  explicit PVecDataType(DLDataType elem_dtype) : elem_dtype_(elem_dtype) {}
 
-  bool Match_(PrimType dtype) const { return dtype.code() == elem_dtype_.code(); }
+  bool Match_(DLDataType dtype) const { return dtype.code == elem_dtype_.code; }
 
  protected:
-  PrimType elem_dtype_;
+  DLDataType elem_dtype_;
 };
 
 /*!
@@ -544,7 +544,7 @@ class PCastExpr : public Pattern<PCastExpr<DType, TA>> {
 
   bool Match_(const ffi::ObjectRef& node) const {
     if (const tirx::CastNode* ptr = node.as<tirx::CastNode>()) {
-      if (!dtype_.Match_(ptr->ty())) return false;
+      if (!dtype_.Match_(ptr->ty()->dtype)) return false;
       if (!value_.Match_(ptr->value)) return false;
       return true;
     } else {
@@ -552,7 +552,7 @@ class PCastExpr : public Pattern<PCastExpr<DType, TA>> {
     }
   }
 
-  PrimExpr Eval() const { return tirx::Cast(dtype_.Eval(), value_.Eval()); }
+  PrimExpr Eval() const { return tirx::Cast(PrimType(dtype_.Eval()), value_.Eval()); }
 
  private:
   typename DType::Nested dtype_;
@@ -562,7 +562,7 @@ class PCastExpr : public Pattern<PCastExpr<DType, TA>> {
 /*!
  * \brief Construct a cast pattern.
  *
- * \param dtype The target data type, can be PVar<PrimType> or PConst<PrimType>.
+ * \param dtype The target data type, can be PVar<DLDataType> or PConst<DLDataType>.
  * \param value The input type.
  *
  * \return The result pattern.
